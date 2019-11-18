986a14209382fe5811d7a8f7228ec71abcf88986
cjsxorud111 <cjsxorud111@gmail.com>
2019-11-18 23:21:13 +0900

    포맷팅

--- src/main/java/com/example/controller/MemberjoinController.java
+++ src/main/java/com/example/controller/MemberjoinController.java
@@ -12,24 +12,24 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class MemberjoinController {
 
-	@Inject
-	private MemberService service;
+    @Inject
+    private MemberService service;
 
-	@RequestMapping(value = "/memberjoin", method = RequestMethod.GET)
-	public String memberjoin() throws Exception {
-		return "memberjoin";
-	}
+    @RequestMapping(value = "/memberjoin", method = RequestMethod.GET)
+    public String memberjoin() throws Exception {
+        return "memberjoin";
+    }
 
-	@RequestMapping(value = "/memberjoining", method = RequestMethod.GET)
-	public String memberjoining(HttpServletRequest request, Model model) throws Exception {
-		String str = request.getParameter("ID");
-		
-		 for(int i=0;i<str.length();i++){
-		     if(Character.getType(str.charAt(i)) == 5) {
-			     return "redirect:memberjoin?han=true";
-		     }     
-		}
-		service.memberjoining(request);
-		return "redirect:/";
-	}
+    @RequestMapping(value = "/memberjoining", method = RequestMethod.GET)
+    public String memberjoining(HttpServletRequest request, Model model) throws Exception {
+        String str = request.getParameter("ID");
+
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.getType(str.charAt(i)) == 5) {
+                return "redirect:memberjoin?han=true";
+            }
+        }
+        service.memberjoining(request);
+        return "redirect:/";
+    }
 }
