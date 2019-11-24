e3c485f8a368719f99cecbc279263d371226136f
cjsxorud111 <cjsxorud111@gmail.com>
2019-11-24 21:37:46 +0900

    자동import

--- src/main/java/com/example/service/MemberService.java
+++ src/main/java/com/example/service/MemberService.java
@@ -3,8 +3,11 @@ package com.example.service;
 import javax.servlet.http.HttpServletRequest;
 
 public interface MemberService {
-	
+
+	public String idValidCheck(String inputId) throws Exception;
+
 	public void memberjoining(HttpServletRequest request) throws Exception;
+
 	public String logining(HttpServletRequest request) throws Exception;
 
 }
--- src/main/java/com/example/service/MemberServiceImpl.java
+++ src/main/java/com/example/service/MemberServiceImpl.java
@@ -7,6 +7,12 @@ import org.springframework.stereotype.Service;
 
 import com.example.dao.MemberDAO;
 import com.example.dto.MemberjoinVO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Service
 public class MemberServiceImpl implements MemberService {
