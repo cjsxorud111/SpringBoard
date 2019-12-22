a7b4ce8ebd930ce718c88348540551818dcf98e4
cjsxorud111 <cjsxorud111@gmail.com>
2019-12-22 20:57:42 +0900

    이미지 업로드 기능 삭제

--- src/main/java/com/example/controller/DefineController.java
+++ src/main/java/com/example/controller/DefineController.java
@@ -9,16 +9,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.*;
 import java.util.List;
 import java.util.Locale;
-import java.util.UUID;
 
 @Controller
 public class DefineController {
@@ -109,7 +104,6 @@ public class DefineController {
 		double pNum = (double)cNum / 10;
 		//나머지 페이지가 있는지 확인
 		double isDecimal = pNum - (int)pNum;
-
 		//총 페이지 개수
 		int totalPageNum;
 		if (isDecimal == 0) {
@@ -132,7 +126,6 @@ public class DefineController {
 		}
 
 		List<memberRankingVO> memberRanking;
-
 		try {
 			memberRanking = service.memberRanking();
 		} catch (Exception e) {
@@ -187,46 +180,5 @@ public class DefineController {
 			e.printStackTrace();
 		}
 		return "redirect:/";
-	}
-	//TODO 추후 img업로드 추가개발에 사용
-	@RequestMapping(value = "img1", method = RequestMethod.POST)
-	public void communityImageUpload1(HttpServletRequest request, HttpServletResponse response,
-			@RequestParam MultipartFile upload) {
-		OutputStream out = null;
-		PrintWriter printWriter = null;
-		response.setCharacterEncoding("utf-8");
-		response.setContentType("text/html;charset=utf-8");
-
-		try {
-			UUID uuid = UUID.randomUUID();
-			String fileName = uuid + "_" + upload.getOriginalFilename();
-			byte[] bytes = upload.getBytes();
-			String uploadPath = "/Users/taeky/eclipse-workspace/SpringBoard/src/main/webapp/resources/img/" + fileName;// 저장경로
-
-			out = new FileOutputStream(new File(uploadPath));
-			out.write(bytes);
-			String callback = request.getParameter("CKEditorFuncNum");
-			printWriter = response.getWriter();
-			String fileUrl = "img/" + fileName; // url경로
-
-			printWriter.println("<script type='text/javascript'>window.parent.CKEDITOR.tools.callFunction(" + callback
-					+ ",'" + fileUrl + "','이미지를 업로드 하였습니다.'" + ")</script>");
-			printWriter.flush();
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (out != null) {
-					out.close();
-				}
-				if (printWriter != null) {
-					printWriter.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		return;
 	}
 }
